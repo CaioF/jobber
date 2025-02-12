b8f45e5eb97b5e9f13a615a4b8696eba8ef8f858
Caio <caio.fleury.r@gmail.com>
2025-02-12 07:23:01 -0300

    Fixed tests dependencies

--- apps/jobber-auth/src/app/app.service.spec.ts
+++ apps/jobber-auth/src/app/app.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 describe('AppService', () => {
   let service: AppService;
 
   beforeAll(async () => {
     const app = await Test.createTestingModule({
-      providers: [AppService],
+      providers: [PrismaService, AppService],
     }).compile();
 
     service = app.get<AppService>(AppService);
