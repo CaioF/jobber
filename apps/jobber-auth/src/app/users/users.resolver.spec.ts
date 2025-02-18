2395aeabc2d94853c1a546bbaccfbc7cd28f274c
Caio <caio.fleury.r@gmail.com>
2025-02-18 07:30:25 -0300

    Fixed tests dependencies

--- apps/jobber-auth/src/app/users/users.resolver.spec.ts
+++ apps/jobber-auth/src/app/users/users.resolver.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver],
+      providers: [PrismaService, UsersService, UsersResolver],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
